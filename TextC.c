7c0e31d85dadc9a3bee93f7dbe5f8e3afadfc567
spectacularalphanumerics <theonicolaou1@protonmail.com>
2025-03-06 20:15:57 -0500

    Update TextC.c

--- TextC.c
+++ TextC.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -9,6 +10,8 @@
 
 /*** data ***/
 struct editorConfig{
+    int screenrows;
+    int screencols;
     struct termios orig_termios;
 };
 
@@ -22,11 +25,16 @@ struct editorConfig E;
 
 int main() {
     enableRawMode();
+    initEditor();
     while (1) {
         editorRefreshScreen();
         editorProcessKeypress();
     }
     return 0
+}
+
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
 /*** terminal ***/
@@ -69,12 +77,32 @@ int getWindowSize(int *rows, int*cols) {
     struct winsize ws;
 
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
         return 0
     }
+}
+
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = "\0";
+    
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;   
 }
 
 
@@ -93,15 +121,51 @@ void editorProcessKeypress() {
 
 /*** output ***/
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
-    editorDrawRows();
-    write(STDOUT_FILENO, "\x1b[H", 3); 
+    struct abuf ab = ABUF_INIT;
+
+    abappend(&ab, "\x1b[?25l",6);
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab); 
 }
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
-    for (y=0;y<24;y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+    for (y=0 ; y < E.screenrows ; y++) {
+        abAppend(ab, "~", 1);
+
+        abAppend(ab, "\x1b[K", 3)
+        if (y < E.screenrows - 1) {
+            abAppend(ab, "\r\n", 2);
+        }
     }
+}
+
+
+/*** append buffer ***/
+//      no dynamic string type so have to make it myself kms
+struct abuf {
+    char *buf;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len],s,len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
 }
