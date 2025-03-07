3b2ec9714eb631e57c75e9e19092a562d60f0e41
spectacularalphanumerics <theonicolaou1@protonmail.com>
2025-03-07 09:00:44 -0500

    Update TextC.c

--- TextC.c
+++ TextC.c
@@ -38,7 +38,11 @@ enum editorKey {
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN,
+    HOME_KEY,
+    END_KEY
 }
 
 /*** init ***/
@@ -100,11 +104,27 @@ int editorReadKey() {
         if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
         if (seq[0] == "[") {
+            if (seq[1] > = '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
+                    }
+                }
+            } else {
             switch (seq[0]) {
                 case "A" : return ARROW_UP;
                 case "B" : return ARROW_DOWN;
                 case "C" : return ARROW_RIGHT;
                 case "D" : return ARROW_LEFT;
+                case "H" : return HOME_KEY;
+                case "F" : return END_KEY;
+                }
             }
         }
         return "\x1b";
@@ -155,6 +175,15 @@ void editorProcessKeypress() {
             exit(0);
             break;
 
+        case PAGE_UP:
+        case PAGE_DOWN:
+        {
+            int times = E.screenrows;
+            while (times --)
+                editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+        break;
+
         case ARROW_UP:
         case ARROW_LEFT:
         case ARROW_DOWN:
@@ -167,16 +196,24 @@ void editorProcessKeypress() {
 void editorMoveCursor(int key) {
     switch (key) {
         case ARROW_LEFT:
-            E.cx--;
+            if (E.cx != 0) {
+                E.cx--;
+            }
             break;
         case ARROW_RIGHT:
-            E.cx++;
+            if (E.xc != E.screencols - 1){
+                E.cx++;
+            }
             break;
         case ARROW_DOWN:
-            E.cy++;
+            if (E.cy != E.screenrows - 1) { 
+                E.cy++;
+            }
             break;
         case ARROW_UP:
-            E.cy--;
+            if (E.cy != 0) {
+                E.cy--;
+            }
             break;
     }
 }
