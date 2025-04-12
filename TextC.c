bd4040ef8fbc7732f718c5026a591171dc475242
spectacularalphanumerics <theonicolaou1@protonmail.com>
2025-04-12 18:11:33 +0300

    Update TextC.c

--- TextC.c
+++ TextC.c
@@ -18,7 +18,7 @@
 #include <time.h>
 
 /*
-        STEP 106
+        STEP 125
         https://viewsourcecode.org/snaptoken/kilo/05.aTextEditor.html
 */
 
@@ -259,22 +259,32 @@ void editorUpdateRow(erow *row) {
     row->rsize = idx;
   }
 
-
-void editorAppendRow(char *s, size_t len) {
-    E.row = realloc(E.row, sizeof(E.row) * (E.numrows + 1));
-
-    int at = E.numrows;
+void editorInsertRow(int at, char *s, size_t len) {
+    if (at < 0 || at > E.numrows) return;
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
     E.row[at].chars[len] = '\0';
-
-    E.row[at].risze = 0;
+    E.row[at].rsize = 0;
     E.row[at].render = NULL;
     editorUpdateRow(&E.row[at]);
-
     E.numrows++;
-    E.dirty;
+    E.dirty++;
+}
+
+void editorFreeRow(erow *row) {
+    free(row->render);
+    free(row->chars);
+}
+
+void editorDelRow(int at) {
+    if (at < 0 || at >= E.numrows) return;
+    editorFreeRow(&E.row[at]);
+    memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - 1 - at));
+    E.numrows--;
+    E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c) {
@@ -285,6 +295,15 @@ void editorRowInsertChar(erow *row, int at, int c) {
     row->chars[at] = c;
     editorUpdateRow(row);
     E.dirty++;      //  Could have used E.dirty = 1 because im just finna treat it as a bool but you can use this to see "how" dirty the buffer is
+}
+
+void editorRowAppendString(erow *row, char *s, size_t len) {
+    row->chars = realloc(row->chars, row->size + len + 1);
+    memcpy(&row->chars[row->size], s, len);
+    row->size += len;
+    row->chars[row->size] = "\0";
+    editorUpdateRow(row);
+    E.dirty++;
 }
 
 void editorRowDelChar(erow *row, int at) {
@@ -300,19 +319,39 @@ void editorRowDelChar(erow *row, int at) {
 
 void editorInsertChar(int c) {
     if (E.cy == E.numrows) {
-        editorAppendRow("", 0);
+        editorInsertRow(E.numrows, "", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
 }
 
+void editorInsertNewline() {
+    if (E.cx == 0) {
+        editorInsertRow(E.cy, "", 0);
+    } else {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
+}
+
 void editorDelChar() {
     if (E.cy == E.numrows) return;
-
+    if (E.cx == 0 && E.cy == 0) return;
     erow *row = &E.row[E.cy];
     if (E.cx > 0) {
         editorRowDelChar(row, E.cx - 1);
         E.cx--;
+    } else {
+        E.cx = E.row[E.cy - 1].size;
+        editorRowAppendString(&E.row[E.cy-1], row->chars, row->size);
+        editorDelRow(E.cy);
+        E.cy--;
     }
 }
 
@@ -330,7 +369,7 @@ void editorOpen(char *filename) {
       while (linelen > 0 && (line[linelen - 1] == '\n' ||
                              line[linelen - 1] == '\r'))
         linelen--;
-      editorAppendRow(line, linelen);
+      editorInsertRow(E.numrows, line, linelen);
     }
     free(line);
     fclose(fp);
