f0afc88d2d5e581dd34c465383251eb114e16e82
spectacularalphanumerics <theonicolaou1@protonmail.com>
2025-05-22 18:52:53 +0300

    Update TextC.c

--- TextC.c
+++ TextC.c
@@ -17,9 +17,38 @@
 #include <sys/types.h>
 #include <time.h>
 
+/*** data ***/
+typedef struct erow {
+    int rsize;
+    char *render;
+    int size;
+    char *chars;
+} erow;
+
+struct editorConfig{
+    int cx, cy;
+    int rx;     //  E.cx is an index into chars field of an erow, E.rx is an index into render field
+    int rowoff;
+    int coloff;
+    int screenrows;
+    int screencols;
+    int numrows;
+    erow *row;
+    char *filename;
+    char statusmsg[80];
+    time_t statusmsg_time;
+    struct termios orig_termios;
+    int dirty;
+};
+
+
+struct editorConfig E;
+
+
+
 /*
-        STEP 125
-        https://viewsourcecode.org/snaptoken/kilo/05.aTextEditor.html
+        STEP 140
+https://viewsourcecode.org/snaptoken/kilo/06.search.html
 */
 
 // Function prototypes
@@ -51,11 +80,13 @@ void editorMoveCursor(int key);
 void editorProcessKeypress();
 
 void editorSetStatusMessage(const char *fmt, ...);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 void editorScroll();
 void editorDrawStatusBar(struct abuf *ab);
 void editorDrawMessageBar(struct abuf *ab);
 void editorDrawRows(struct abuf *ab);
 void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 void abAppend(struct abuf *ab, const char *s, int len);
 void abFree(struct abuf *ab);
@@ -63,32 +94,6 @@ void abFree(struct abuf *ab);
 void initEditor();
 
 
-/*** data ***/
-typedef struct erow {
-    int rsize;
-    char *render;
-    int size;
-    char *chars;
-} erow;
-
-struct editorConfig{
-    int cx, cy;
-    int rx;     //  E.cx is an index into chars field of an erow, E.rx is an index into render field
-    int rowoff;
-    int coloff;
-    int screenrows;
-    int screencols;
-    int numrows;
-    erow *row;
-    char *filename;
-    char statusmsg[80];
-    time_t statusmsg_time;
-    struct termios orig_termios;
-    int dirty;
-};
-
-
-struct editorConfig E;
 
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f)
@@ -124,7 +129,7 @@ int main(int argc, char *argv[]) {
         editorOpen();
     }
 
-    editorSetStatusMessage("HELP: Ctrl-Q = quit | Ctrl-s = save");
+    editorSetStatusMessage("HELP: Ctrl-Q = quit | Ctrl-s = save | CTRL-F = find");
 
     while (1) {
         editorRefreshScreen();
@@ -269,6 +274,19 @@ int editorCxToRx(erow *row, int cx) {
         rx++;
     }
     return rx;
+}
+
+int editorRowRxToCx(erow *row, int rx) {
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0 ; cx < row->size ; cx++) {
+        if (row->chars[cx] == '\t') {
+            cur_rx += (TEXTC_TAB_STOP - 1) - (cur_rx * TEXTC_TAB_STOP);
+        }
+        cur_rx++;
+        if (cur_rx == rx) return cx;
+    }
+    return cx;
 }
 
 void editorUpdateRow(erow *row) {
@@ -429,7 +447,13 @@ char *editorRowsToString(int *buflen) {
 }
 
 void editorSave() {
-    if (E.filename == NULL) return;
+    if (E.filename == NULL) {
+        E.filename = editorPrompt("save as: %s (ESC to cancel)", NULL);
+        if (E.filename == NULL) {
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
+    }
     int len;
     char *buf = editorRowsToString(&len);
     int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
@@ -449,8 +473,101 @@ void editorSave() {
     editorSetStatusMessage("Can't save, I/O error: %s", strerror(errno));
 }
 
+/*** find ***/
+
+
+void editorFindCallback(char *query, int key) {
+    static int last_match = -1;
+    static int direction = 1;
+
+    if (key == '\r' || key == '\x1b') {
+        last_match = -1;
+        direction = 1;
+        return;
+    } else if (key == ARROW_RIGHT || key == ARROW_LEFT) {
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
+    }
+
+    int i;
+    for (i=0; i<E.numrows; i++) {
+        erow *row = &E.row[i];
+        char *match = strstr(row->render, query);
+        if (match) {
+            E.cy = i;
+            E.cx = editorRowRxToCx(row, match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+}
+
+void editorFind() {
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloff = E.coloff;
+    int saved_rowoff = E.rowoff;
+
+    char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+    if (query) {
+        free(query);
+    } else {
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.coloff = saved_coloff;
+        E.rowoff = saved_rowoff;
+    }
+}
+
+
+
+
+
 
 /*** input ***/
+
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = "\0";
+    while (1) {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == DEL_KEY || c == CTRL_KEY("h") || c == BACKSPACE) {
+            if (buflen != 0) buf[--buflen] = '\0';
+        } else if (c == "\x1b") {
+            editorSetStatusMessage("");
+            if (callback) callback(buf, c);
+            free(buf);
+            return NULL;
+        } else if (c == "\r") {
+            if (buflen != 0) {
+                editorSetStatusMessage("");
+                if (callback) callback(buf, c);
+                return buf;
+            }
+        } else if (!iscntrl(c) && c < 128) {
+            if (buflen == bufsize - 1) {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+
+        if (callback) callback(buf, c);
+    }
+}
+
+
 
 void editorMoveCursor(int key) {
     erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
@@ -495,7 +612,7 @@ void editorProcessKeypress() {
     int c = editorReadKey();
     switch (c) {
         case '\r':
-            /* TODO */
+            editorInsertNewline();
                 break;
         case CTRL_KEY('q'):
             if (E.dirty && quit_times > 0) {
@@ -510,6 +627,10 @@ void editorProcessKeypress() {
 
         case CTRL_KEY('s'):
             editorSave();
+            break;
+
+        case CTRL_KEY('f'):
+            editorFind();
             break;
 
         case HOME_KEY:
@@ -652,7 +773,7 @@ void editorRefreshScreen() {
     editorScroll();
     struct abuf ab = ABUF_INIT;
 
-    abappend(&ab, "\x1b[?25l",6);
+    abAppend(&ab, "\x1b[?25l",6);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
